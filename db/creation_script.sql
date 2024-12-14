5f3bb92f9a21298f738eb210f273ff9d1694785a
Franco Stazzone <fralstazzone@gmail.com>
2024-12-14 03:02:04 -0300

    Adds stored procedures for board and thread insertion

--- db/creation_script.sql
+++ db/creation_script.sql
@@ -2,6 +2,8 @@ CREATE DATABASE BoardCrawler
 
 USE BoardCrawler
 
+-- Create tables
+
 CREATE TABLE Board (
     id DECIMAL(4) PRIMARY KEY IDENTITY(1,1),
     board_name NVARCHAR(10) NOT NULL,
@@ -45,3 +47,37 @@ CREATE TABLE ReplyMentions (
     FOREIGN KEY (board_id, reply_id) REFERENCES Reply(board_id, reply_id),
     FOREIGN KEY (board_id, mentioned_reply) REFERENCES Reply(board_id, reply_id)
 )
+GO
+
+-- Create functions
+
+-- Find board by name
+CREATE FUNCTION findBoardId(@board_name NVARCHAR(10))
+RETURNS DECIMAL(4)
+AS
+BEGIN
+	RETURN (SELECT b.id FROM Board b WHERE b.board_name = @board_name)
+END
+GO
+
+-- Create store procedures
+
+-- Board insertion
+CREATE PROCEDURE uspInsertBoard(@board_name NVARCHAR(10), @board_title NVARCHAR(50)) AS
+BEGIN
+	IF dbo.findBoardId(@board_name) IS NULL
+	BEGIN
+		INSERT INTO Board (board_name, title) VALUES (@board_name, @board_title)
+		RETURN 0 -- all good
+	END
+
+	RETURN 1 -- the board is already in the db
+END
+
+CREATE PROCEDURE uspInsertThread(@board_name NVARCHAR(10), @thread_number DECIMAL(18), @title NVARCHAR(256)) AS
+BEGIN
+	DECLARE @board_id DECIMAL(4)
+	SET @board_id = dbo.findBoardId(@board_name)
+
+	INSERT INTO Thread (board_id, thread_number, title) VALUES (@board_id, @thread_number, @title)
+END
